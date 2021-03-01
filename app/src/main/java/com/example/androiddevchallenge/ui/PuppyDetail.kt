4787fa5c260c39c1be8c9319e54d17f7652e8d2e
raodunwei <raodunwei@taqu.cn>
2021-03-01 16:56:15 +0800

    fix bug

--- app/src/main/java/com/example/androiddevchallenge/ui/PuppyDetail.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/PuppyDetail.kt
@@ -2,7 +2,12 @@ package com.example.androiddevchallenge.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
